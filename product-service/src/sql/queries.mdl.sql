dfdcdeaca8c253f826fbedffe012d942201425a3
Leonid Petrov <leonid.petrov@latoken.com>
2020-11-07 15:38:46 +0300

    feat: add query get product by id

--- product-service/src/sql/queries.mdl.sql
+++ product-service/src/sql/queries.mdl.sql
@@ -1,2 +1,7 @@
 select p.id, p.title, p.description, p.price, s.count from products p
 inner join stocks s on p.id  = s.product_id 
+
+select p.id, p.title, p.description, p.price, s.count 
+from products p
+inner join stocks s on p.id  = s.product_id
+where p.id='50c36780-b33c-4b9c-93bd-46f3b9b3f5cb'
