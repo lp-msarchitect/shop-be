09230ff0f5759e0507e12588d61a56083324cd54
Leonid Petrov <leonid.petrov@latoken.com>
2020-11-10 00:07:47 +0300

    feat: add babel es6

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,3 +1,4 @@
+'use strict';
 import { getProductById } from './src/get-product-by-id.js';
 import { getAllProducts } from './src/get-all-products.js';
 import { postProduct } from './src/post-products';
--- product-service/webpack.config.js
+++ product-service/webpack.config.js
@@ -1,9 +1,21 @@
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 module.exports = {
   entry: slsw.lib.entries,
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   target: 'node',
-  externals: [nodeExternals()] // exclude external modules
+  externals: [nodeExternals()], // exclude external modules
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
